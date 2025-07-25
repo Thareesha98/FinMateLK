01edaff0000f3afff0dcb697f1d6ca05da472ad7
Thareesha <thareesha98>
2025-07-25 19:02:43 +0530

    Project Initialized

---
+++ BCS/CareerProject/src/main/java/thareesha/career/CareerProject/CareerProjectApplication.java
@@ -0,0 +1,13 @@
+package thareesha.career.CareerProject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CareerProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CareerProjectApplication.class, args);
+	}
+
+}
---
+++ BCS/CareerProject/src/test/java/thareesha/career/CareerProject/CareerProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package thareesha.career.CareerProject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CareerProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
